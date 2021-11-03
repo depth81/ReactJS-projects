5a833f55f173ae5d5d9a3bdb336ee7e184ec1f4e
depth81 <42650880+depth81@users.noreply.github.com>
2021-11-02 21:07:33 -0500

    update project

---
+++ zero-app/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, SafeAreaView, Image } from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+        <Text>HELLO PAULO ENRIQUE!</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
