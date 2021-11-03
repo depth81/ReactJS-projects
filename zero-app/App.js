61181ef2bd56c9bd4efd16cd3c355546bd897288
depth81 <petv1981@gmail.com>
2021-11-02 22:59:27 -0500

    fixing

--- zero-app/App.js
+++ zero-app/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { View, Text, SafeAreaView, Image } from 'react-native'
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text, SafeAreaView, Image} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView>
-        <Text>HELLO PAULO ENRIQUE!</Text>
+      <Text>HELLO PAULO ENRIQUE! HOW ARE YOU?</Text>
+      <Text>I'm pretty fine, thanks for asking, what about you?</Text>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default App
+export default App;
