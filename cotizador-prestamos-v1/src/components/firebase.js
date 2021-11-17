d3ef92471455416a6682ea502bde2cb22473e9af
401 <401@academicos.cesde.edu.co>
2021-11-16 20:50:02 -0500

    update

---
+++ cotizador-prestamos-v1/src/components/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD9-VXBtBJt503fgIiVCPOUnmPC3tcVn5Y",
+    authDomain: "cotizador-prestamos-v1.firebaseapp.com",
+    projectId: "cotizador-prestamos-v1",
+    storageBucket: "cotizador-prestamos-v1.appspot.com",
+    messagingSenderId: "334999474930",
+    appId: "1:334999474930:web:34957f5fbeefb78c61f66b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
