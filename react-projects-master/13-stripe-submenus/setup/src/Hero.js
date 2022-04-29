1bdcc8df55a81c3b7c964d126ccb95cfc061c0b2
depth81 <petv1981@gmail.com>
2022-04-28 22:00:19 -0500

    update
    ts -> 7:40:02
    https://www.youtube.com/watch?v=a_7Z7C_JCyo&t=20692s

--- react-projects-master/13-stripe-submenus/setup/src/Hero.js
+++ react-projects-master/13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
+import { useGlobalContext } from './context'
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {closeSubmenu} = useGlobalContext();
+  return(
+    <section className='hero' onMouseOver={closeSubmenu}>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>Payments infrastructure of the internet</h1>
+          <p>
+          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+          <button className='btn'>
+            
+          </button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} className='phone-img' alt='phone' />
+        </article>
+      </div>
+    </section>
+  ) 
 }
 
 export default Hero
