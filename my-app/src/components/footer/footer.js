2bb7f402b3147bdc16a64d4ddad7ac889be7d1fe
depth81 <petv1981@gmail.com>
2022-06-24 14:40:21 -0500

    update

--- my-app/src/components/footer/footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = ({year}) =>{
-    return(
-        <div className="jumbotron text-center" style={{"marginBottom":"0"}}>
-            <p>TODOS LOS DERECHOS RESERVADOS {year}</p>
-        </div>
-    );
-};
